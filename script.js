e98140d86451a8520e09a55f1b05cf7c884fd0a5
nadeem-elbarbari <nadeem.elbarbari@gmail.com>
2023-12-11 23:17:22 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+// Array of quotes
+var arrayOfQuotes = [
+    {
+        quote: "Be yourself; everyone else is already taken",
+        author: "Oscar Wilde"
+    },
+    {
+        quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe",
+        author: "Albert Einstein"
+    },
+    {
+        quote: "So many books, so little time",
+        author: "Frank Zappa"
+    },
+    {
+        quote: "A room without books is like a body without a soul",
+        author: "Marcus Tullius Cicero"
+    },
+    {
+        quote: "Be who you are and say what you feel, because those who mind don't matter and those who matter don't mind",
+        author: "Bernard M. Baruch"
+    },
+    {
+        quote: "You only live once, but if you do it right, once is enough",
+        author: "Mae West"
+    },
+    {
+        quote: "Live as if you were to die tomorrow. Learn as if you were to live forever",
+        author: "Mahatma Gandhi"
+    }
+];
+
+// Function to display new quote
+function newQuote() {
+  var randomNumber = Math.floor(Math.random() * arrayOfQuotes.length);
+  document.getElementById("quoteDisplay").innerHTML =
+    '"' + arrayOfQuotes[randomNumber].quote + '"';
+  document.getElementById("authorDisplay").innerHTML =
+    "-- " + arrayOfQuotes[randomNumber].author;
+}
+
+document.getElementById("newQuote").onclick = newQuote;
